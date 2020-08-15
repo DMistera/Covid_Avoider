a77b3223cbfe2496dfdbff39923ff79b9c867e41
NadiraX <57359646+NadiraX@users.noreply.github.com>
2020-08-15 17:19:35 +0200

    main animation +history

--- app/src/main/java/com/example/covidavoider/MainActivity.java
+++ app/src/main/java/com/example/covidavoider/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.covidavoider;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.tomer.fadingtextview.FadingTextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FadingTextView fadingTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fadingTextView = findViewById(R.id.fading_text_view);
+    }
+    public void startExample2(View v){
+        String[] example2 = {"Logowanie","lub","rejestracja"};
+        fadingTextView.setTexts(example2);
+        fadingTextView.setTimeout(1000, TimeUnit.MILLISECONDS);
     }
 }
